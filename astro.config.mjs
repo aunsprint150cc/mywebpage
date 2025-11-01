20425dab40a0614d9273d3f298c9340d24b30dc0
aunsprint <rjeer43@gmail.com>
2025-11-01 14:38:12 +0700

    change to astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://aunsprint150cc.github.io',
+    base: '/mywebpage',
+});
